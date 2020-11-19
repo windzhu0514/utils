ad681b58c8f923bed5054ee877d16573d3301330
windzhu0514 <windzhu0514@163.com>
2020-11-19 20:36:20 +0800

    任务管理模块

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/windzhu0514/go-utils
+
+go 1.14
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/robfig/cron/v3 v3.0.1
+	github.com/satori/go.uuid v1.2.0
+	github.com/skoo87/log4go v0.0.0-20160718120903-4be49e76f196
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+)
---
+++ taskmanager/taskmanager.go
@@ -0,0 +1,191 @@
+package taskmanager
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/robfig/cron/v3"
+)
+
+type Task interface {
+	Init() error
+	Run(ctx JobContext)
+	Worker(ctx JobContext)
+	Release() error
+}
+type TaskOptions struct {
+	WorkerNum      int
+	CronExpression string
+	StartNow       bool
+}
+
+type taskManager struct {
+	mux  sync.Mutex
+	jobs map[string]*Job
+	Cron *cron.Cron
+}
+
+type TaskStatus int
+
+const (
+	StatusStopped TaskStatus = iota
+	StatusStopping
+	StatusRunning
+	StatusWaiting
+)
+
+type Job struct {
+	JobContext
+
+	opts    TaskOptions
+	task    Task
+	status  TaskStatus
+	entryID cron.EntryID
+
+	wgWorker sync.WaitGroup
+}
+
+type JobContext struct {
+	Key  string
+	Stop chan struct{}
+}
+
+var manager taskManager
+
+// Run implements cron Job interface
+func (job *Job) Run() {
+	if err := job.task.Init(); err != nil {
+		return
+	}
+
+	for i := 0; i < job.opts.WorkerNum; i++ {
+		job.wgWorker.Add(1)
+		go func() {
+			job.task.Worker(job.JobContext)
+			job.wgWorker.Done()
+		}()
+	}
+
+	job.task.Run(job.JobContext)
+	job.wgWorker.Wait()
+	delete(manager.jobs, job.JobContext.Key)
+
+	return
+}
+
+func Start(key string, opts TaskOptions, task Task) error {
+	_, ok := getJob(key)
+	if ok {
+		return errors.New("task is already registered")
+	}
+
+	job := &Job{opts: opts, task: task}
+
+	if job.status != StatusStopped {
+		return errors.New("task is already running")
+	}
+
+	// 定时任务
+	if job.opts.CronExpression != "" {
+		if manager.Cron == nil {
+			manager.Cron = cron.New(cron.WithChain(cron.SkipIfStillRunning(nil))) // TODO:
+			manager.Cron.Start()
+		}
+
+		entryID, err := manager.Cron.AddJob(job.opts.CronExpression, job)
+		if err != nil {
+			return err
+		}
+
+		job.entryID = entryID
+
+		if job.opts.StartNow {
+			job.Run()
+		}
+
+		return nil
+	}
+
+	go job.Run()
+
+	manager.jobs[key] = job
+
+	return nil
+}
+
+func Stop(key string) error {
+	job, ok := getJob(key)
+	if !ok {
+		return errors.New("task is already stopped")
+	}
+
+	if job.status != StatusRunning {
+		return errors.New("task is not running")
+	}
+
+	close(job.Stop)
+	return nil
+}
+
+func Status(key string) TaskStatus {
+	job, ok := getJob(key)
+	if !ok {
+		return StatusStopped
+	}
+
+	return job.status
+}
+
+func IncreaseWorker(key string) error {
+	job, ok := getJob(key)
+	if !ok {
+		return errors.New("task is not running")
+	}
+
+	if job.status != StatusRunning {
+		return errors.New("task is not running")
+	}
+
+	job.wgWorker.Add(1)
+	job.opts.WorkerNum++
+	go func() {
+		job.task.Worker(job.JobContext)
+		job.wgWorker.Done()
+	}()
+
+	return nil
+}
+
+func DecreaseWorker(key string) error {
+	job, ok := getJob(key)
+	if !ok {
+		return errors.New("task is not running")
+	}
+
+	if job.status != StatusRunning {
+		return errors.New("task is not running")
+	}
+
+	job.status = StatusStopping
+
+	go func() { job.Stop <- struct{}{} }()
+
+	return nil
+}
+
+func getJob(key string) (*Job, bool) {
+	_, ok := manager.jobs[key]
+	if !ok {
+		return nil, false
+	}
+
+	manager.mux.Lock()
+	defer manager.mux.Unlock()
+
+	job, ok := manager.jobs[key]
+	if !ok {
+		return nil, false
+	}
+
+	return job, true
+}
